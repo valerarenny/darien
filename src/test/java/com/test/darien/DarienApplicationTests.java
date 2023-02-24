c0b4216c3aff8e3195217885b006d9ba7846ff1e
valerarenny <valerarenny@gmail.com>
2023-02-24 14:29:12 -0400

    primer commit

---
+++ src/test/java/com/test/darien/DarienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.darien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
